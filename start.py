a7f471a7c9db13e24b2e76038486f9fb6f480d5c
Khang Truong <113339215+Khang2910@users.noreply.github.com>
2025-07-25 14:30:30 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,14 @@
 fnames = ['victim.go', 'prepare.sh', 'serving/content.bat']
 
-for fname in fnames:
-    with open(fname) as f:
-        content = f.read()
-    with open(fname, 'w') as f:
-        new_content = content.replace('192.168.2.135', 'newip')
-        f.write(new_content)
+newip = 'newip'
+if newip == 'newip':
+    print('Set it now')
+else:
+    for fname in fnames:
+        with open(fname) as f:
+            content = f.read()
+        with open(fname, 'w') as f:
+            new_content = content.replace('192.168.2.135', newip)
+            f.write(new_content)
 
   
